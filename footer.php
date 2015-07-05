9be1729ae0dcc6028b47aecf9ce74970f0a8a780
ayadibaha <ayadibaha51@gmail.com>
2015-07-05 03:03:32 +0100

    Last step

---
+++ footer.php
@@ -0,0 +1,48 @@
+<div class="row quicklinks">
+  <div class="container">
+    <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+      <div class="row">
+        <div class="container">
+          <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 add"><h5 class="hlm"><span class="glyphicon glyphicon-map-marker round"></span>Heritage Law Marketing</h5><p>2300 Richmond Avenue #215<br>Houston, Texas 77098<br>(540) 383-5557<br>support@heritagelawmarketing.com</p></div>
+          <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+            <h5><span class="glyphicon glyphicon-link round"></span>Quick Links</h5>
+                <ul>
+                  <li><a href="index.php"><span class="glyphicon glyphicon-chevron-right"></span> Home</a></li>
+                  <li><a href="about.php"><span class="glyphicon glyphicon-chevron-right"></span> About</a></li>
+                  <li><a href=""><span class="glyphicon glyphicon-chevron-right"></span> Portfolio</a></li>
+                  <li><a href=""><span class="glyphicon glyphicon-chevron-right"></span> Services</a></li>
+                  <li><a href=""><span class="glyphicon glyphicon-chevron-right"></span> Sign Up</a></li>
+                  <li><a href="contact.php"><span class="glyphicon glyphicon-chevron-right"></span> Contact</a></li>
+                </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+      <div class="row"><img class="img-responsive sig" src="files/sig.png"></img></div>
+      <div class="row icons">
+        <a class="round" href="#"><i class="fa fa-facebook"></i></a>
+        <a class="round" href="#"><i class="fa fa-twitter"></i></a>
+        <a class="round" href="#"><i class="fa fa-linkedin"></i></a>
+        <a class="round" href="#"><i class="fa fa-google"></i></a>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row foot">
+  <div class="container">
+    <p>2015 Heritage Law Marketing, LLC. All Right Reserved</p>
+    <div class="pull-right">
+      <img class="img-responsive" style="margin-left:-60%;" src="files/bbb.png"></img>
+      <img class="img-responsive" style="width:109px" src="files/comodo.png"></img>
+    </div>
+  </div>
+</div>
+
+
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Heritage Law Marketing</title>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="files/style.css">
+<link rel="stylesheet" type="text/css" href="files/bootstrap-social.css">
+<link rel="stylesheet" type="text/css" href="files/font-awesome.css">
+</head>
+<body>
+<div class="container">
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a href="#">
+        <img alt="Brand" src="files/logo.png">
+      </a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="navbar">
+      <ul class="nav navbar-nav">
+        <li><a href="index.php">Home <span class="sr-only">(current)</span></a></li>
+        <li role="separator" class="divider"></li>
+        <li><a href="directories.php">Directories</a></li>
+        <li role="separator" class="divider"></li>
+        <li><a href="leads.php">Leads</a></li>
+        <li role="separator" class="divider"></li>
+        <li><a href="contact.php">Contact</a></li>
+        <li class="phone"><span class="glyphicon glyphicon-earphone"></span><h4>(540) 383-5557</h4></li>
+        <li><button class="btn signin"><img class="img-responsive" src="files/key.png"></img>SIGN IN</button></li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
